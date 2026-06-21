7d01f0d7f09e70466bf138d62669d942441b308d
NeilNNP5-dev <neilnnp45@gmail.com>
2026-06-22 00:40:13 +0530

    Integrated OOP (Class for all the Celestial Bodies)

--- test1.py
+++ test1.py
@@ -13,53 +13,53 @@ clock = pygame.time.Clock()
 pygame.display.set_caption("Pygame Playground")
 
 running = True
-planet1_x=400
-planet1_y=150
-planet2_x=400
-planet2_y= 50
-sun_x = WIDTH//2
-sun_y = HEIGHT//2
-vx1 = 9
-vy1 = 0
-vx2 = 11
-vy2 = 0
-ax1 = 0.5
-ay1 = 0.5
-ax2 = 0.5
-ay2 = 0.5
-G = 100
+class Body:
+    def __init__(self,x,y,vx,vy,color,radius,mass):
+        self.x = x
+        self.y = y
+        self.vx = vx
+        self.vy = vy
+        self.color = color
+        self.radius = radius
+        self.mass = mass
+planet1 = Body(400,150,9,0,(0,0,255),25,100)
+sun = Body(400,300,0,0,(255,255,0),50,10000000)
+planet2 = Body(400,50,11,0,(255,0,0),30,5000)
+ax = 0.5
+ay = 0.5
+G= 100
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
     screen.fill((0,0,0))
-    dx1 = sun_x - planet1_x
-    dy1 = sun_y - planet1_y
-    dx2 = sun_x - planet2_x
-    dy2 = sun_y - planet2_y 
+    dx1 = sun.x - planet1.x
+    dy1 = sun.y - planet1.y
+    dx2 = sun.x - planet2.x
+    dy2 = sun.y - planet2.y 
     distance1 = math.sqrt(dx1**2 + dy1**2 + 100)
     distance2 = math.sqrt(dx2**2 + dy2**2 + 100)
-    vx1 += ax1 * (dx1/distance1)
-    planet1_x +=  vx1  
-    vy1 += ay1 * (dy1/distance1)
-    planet1_y += vy1
-    vx2 += ax2 * (dx2/distance2)
-    planet2_x +=  vx2  
-    vy2 += ay2 * (dy2/distance2)
-    planet2_y += vy2   
-    pygame.draw.circle(screen,(0,0,255),(planet1_x, planet1_y), 25)
-    pygame.draw.circle(screen, (255,255,0), (sun_x, sun_y), 50 )
-    pygame.draw.circle(screen,(100,10,0),(planet2_x, planet2_y), 30)
-    if planet1_x+25>=WIDTH or planet1_x-25<=0:
-        vx1*= -1
-    if planet1_y+25>=HEIGHT or planet1_y-25<=0:
-        planet1_y = HEIGHT-25
-        vy1*= -1
-    if planet2_x+30>=WIDTH or planet2_x-30<=0:
-        vx2*= -1
-    if planet2_y+30>=HEIGHT or planet2_y-30<=0:
-        planet2_y = HEIGHT-35
-        vy2*= -1    
+    planet1.vx += ax * (dx1/distance1)
+    planet1.x +=  planet1.vx  
+    planet1.vy += ay * (dy1/distance1)
+    planet1.y += planet1.vy
+    planet2.vx += ax * (dx2/distance2)
+    planet2.x +=  planet2.vx  
+    planet2.vy += ay * (dy2/distance2)
+    planet2.y += planet2.vy   
+    pygame.draw.circle(screen,planet1.color,(planet1.x, planet1.y), planet1.radius)
+    pygame.draw.circle(screen, sun.color, (sun.x, sun.y), sun.radius )
+    pygame.draw.circle(screen,planet2.color,(planet2.x, planet2.y), planet2.radius)
+    if planet1.x+25>=WIDTH or planet1.x-25<=0:
+        planet1.vx*= -1
+    if planet1.y+25>=HEIGHT or planet1.y-25<=0:
+        planet1.y = HEIGHT-25
+        planet1.vy*= -1
+    if planet2.x+30>=WIDTH or planet2.x-30<=0:
+        planet2.vx*= -1
+    if planet2.y+30>=HEIGHT or planet2.y-30<=0:
+        planet2.y = HEIGHT-35
+        planet2.vy*= -1    
     pygame.display.flip()
     clock.tick(60)
 pygame.quit()
