80b94920cc13f230e1b5dca3d1cd8b5cb6fd6499
NeilNNP5-dev <neilnnp45@gmail.com>
2026-06-24 19:27:33 +0530

    Reverted Planet Masses to normal values (was testing earlier)

--- orbit_simulation.py
+++ orbit_simulation.py
@@ -25,10 +25,10 @@ class Body:
         self.radius = radius
         self.mass = mass
         self.trail = []
-planet1 = Body(500,300,6.1,0,(0,0,255),10,10)
+planet1 = Body(500,300,6.1,0,(0,0,255),10,1)
 sun = Body(500,500,0,0,(255,255,0),30,7500)
-planet2 = Body(500,100,4.3,0,(255,0,0),15,50)
-planet3 = Body(500,200,5,0,(180,0,255),12.5,30)
+planet2 = Body(500,100,4.3,0,(255,0,0),15,5)
+planet3 = Body(500,200,5,0,(180,0,255),12.5,1)
 bodies = [sun,planet1,planet2,planet3]
 G= 1
 def update_physics(bodies):
