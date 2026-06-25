2e20d4f2e352a8a6b3008915f4280c06ee2f0096
NeilNNP5-dev <neilnnp45@gmail.com>
2026-06-25 13:51:26 +0530

    Added Zoom in and Zoom out feature

--- orbit_simulation.py
+++ orbit_simulation.py
@@ -7,7 +7,10 @@ pygame.init()
 
 WIDTH = 1000
 HEIGHT = 1000
-MAX_TRAIL = 600
+MAX_TRAIL = 580
+zoom = 1.0
+camera_x = WIDTH/2
+camera_y = HEIGHT/2
 
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -28,7 +31,7 @@ class Body:
 planet1 = Body(500,300,6.1,0,(0,0,255),10,1)
 sun = Body(500,500,0,0,(255,255,0),30,7500)
 planet2 = Body(500,100,4.3,0,(255,0,0),15,5)
-planet3 = Body(500,200,5,0,(180,0,255),12.5,1)
+planet3 = Body(500,200,5,0,(180,0,255),12.5,3)
 bodies = [sun,planet1,planet2,planet3]
 G= 1
 def update_physics(bodies):
@@ -57,12 +60,25 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_i]:
+            zoom*= 1.01
+    if keys[pygame.K_o]:
+            zoom/= 1.01       
     screen.fill((0,0,0))
     update_physics(bodies)
     for body in bodies:
+        sx = (body.x - camera_x)* zoom + WIDTH/2
+        sy = (body.y - camera_y)* zoom + HEIGHT/2
         for i in range(len(body.trail)-1):
-                pygame.draw.line(screen,body.color,body.trail[i],body.trail[i+1],2)
-        pygame.draw.circle(screen,(body.color),(body.x,body.y),body.radius)
+                x1, y1 = body.trail[i]
+                x2, y2 = body.trail[i+1]
+                sx1 = (x1 - camera_x) * zoom + WIDTH/2
+                sy1 = (y1 - camera_y) * zoom + HEIGHT/2
+                sx2 = (x2 - camera_x) * zoom + WIDTH/2
+                sy2 = (y2 - camera_y) * zoom + HEIGHT/2
+                pygame.draw.line(screen,body.color,(sx1,sy1),(sx2,sy2),2)        
+        pygame.draw.circle(screen,(body.color),(sx,sy),body.radius)
     pygame.display.flip()
     clock.tick(60)
 pygame.quit()
